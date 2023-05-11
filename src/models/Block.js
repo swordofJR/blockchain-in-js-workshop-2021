a9f1cc277b359164866657bb9948cd58b09c5723
JR <1359684233@qq.com>
2023-05-11 19:58:24 +0800

    test02

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,17 +1,32 @@
+// const sha256 = require('crypto-js/sha256')
 import sha256 from 'crypto-js/sha256.js'
-
-export const DIFFICULTY = 2
 
 class Block {
   // 1. 完成构造函数及其参数
+  /* 构造函数需要包含
 
-  constructor() {}
+  */
+  constructor(blockchain, parentHash, height, hash) {
+    this.blockchain = blockchain;
+    this.parentHash = parentHash;
+    this.height = height;
+    this.hash = hash
+  }
 
-  isValid() {}
+  getPreviousBlock() {
+    return this.blockchain.blocks[this.parentHash]
+}
 
-  setNonce(nonce) {}
-  
+isValid() {
+  const leadingZero = '0'.repeat(DIFFICULTY)
+
+  return (this.nonce+"").startsWith(leadingZero)
+}
+
+setNonce(nonce) {
+  this.nonce = nonce
+}
 }
 
 export default Block
-
+export const DIFFICULTY = 3
--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -1,4 +1,5 @@
 // Blockchain
+import { maxBy, reduce, reverse, unfold, values, prop } from "ramda";
 class Blockchain {
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含 
@@ -6,17 +7,36 @@ class Blockchain {
       - 创世区块
       - 存储区块的映射
   */
-  constructor() {}
+      constructor(name) {
+        this.name = name
+        this.genesis = null
+        this.blocks = {}
+        if (this.genesis) {
+          this.blocks[this.genesis.hash] = this.genesis
+        }
+      }
 
   // 2. 定义 longestChain 函数
   /* 
     返回当前链中最长的区块信息列表
   */
-  longestChain() {
 
-
-    return []
-  }
+    longestChain() {
+      let longestChain = []
+      for(let hash in this.blocks) {
+        let currBlock = this.blocks[hash]
+        let chain = [currBlock]
+        while(currBlock.parentHash != 'root' && currBlock.hash != 'root') {
+          currBlock = currBlock.getPreviousBlock()
+          chain.unshift(currBlock)
+        }
+        if(chain.length > longestChain.length) {
+          longestChain = chain
+        }
+      }
+      return longestChain
+    }
+ 
 }
 
 export default Blockchain
