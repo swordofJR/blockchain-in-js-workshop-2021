4725ec69f955f766075cade8dd1122c14e8907b2
JR <1359684233@qq.com>
2023-05-21 15:24:30 +0800

    test03

--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -1,6 +1,7 @@
-import UTXOPool from './UTXOPool.js'
+import UTXO from './UTXO.js' 
+import UTXOPool from './UTXOPool.js' 
 
-Blockchain
+// Blockchain
 class Blockchain {
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含
@@ -8,25 +9,51 @@ class Blockchain {
       - 创世区块
       - 存储区块的映射
   */
-  constructor() {}
+      constructor(name) {
+        this.name = name
+        this.genesis = null
+        this.blocks = {}
+        if (this.genesis) {
+          this.blocks[this.genesis.hash] = this.genesis
+        }
+      }
 
   // 2. 定义 longestChain 函数
-  /*
+  /* 
     返回当前链中最长的区块信息列表
   */
-  longestChain() {
-    return []
-  }
+    longestChain() {
+      let longestChain = []
+      for(let hash in this.blocks) {
+        let currBlock = this.blocks[hash]
+        let chain = [currBlock]
+        while(currBlock.parentHash != 'root' && currBlock.hash != 'root') {
+          currBlock = currBlock.getPreviousBlock()
+          chain.unshift(currBlock)
+        }
+        if(chain.length > longestChain.length) {
+          longestChain = chain
+        }
+      }
+      return longestChain
+    }
 
   // 判断当前区块链是否包含
   containsBlock(block) {
-    // 添加判断方法
-    return false
+    return this.blocks[block.hash] != null
   }
 
   // 获得区块高度最高的区块
   maxHeightBlock() {
     // return Block
+    let maxHeightBlock = this.blocks.element(0)
+    for (let hash in this.blocks) {
+      currentBlock = this.blocks[hash]
+      if (currentBlock.height > maxHeightBlock.height) {
+        maxHeightBlock = currentBlock
+      }
+    }
+    return maxHeightBlock
   }
 
   // 添加区块
@@ -36,8 +63,16 @@ class Blockchain {
   _addBlock(block) {
     if (!block.isValid()) return
     if (this.containsBlock(block)) return
-
+    // 将新区块添加至blocks
+    this.blocks[block.hash] = block
     // 添加 UTXO 快照与更新的相关逻辑
+    // 复制新区块前一个区块交易池
+    var preUTXOPool = block.getPreviousBlock().utxoPool.clone()
+    block.utxoPool.utxos = preUTXOPool
+    // 添加创币交易
+    let coinbaseUTXO = new UTXO(block.coinbaseBeneficiary,12.5)
+    // 在交易池添加该笔交易
+    block.utxoPool._addUTXO(coinbaseUTXO)
   }
 }
 
--- src/models/UTXOPool.js
+++ src/models/UTXOPool.js
@@ -1,16 +1,29 @@
-import UTXO from './UTXO.js'
+import './UTXO.js'
 
 class UTXOPool {
-  constructor(utxos = {}) {}
+  constructor(utxos = {}) {
+    this.utxos = utxos
+  }
 
   // 添加交易函数
   /**
    * 将交易的信息更新至 UTXOPool 中
    */
-  addUTXO() {}
+  _addUTXO(utxo) {
+    // 根据公钥判断当前utxo池是否为空
+    // 如果utxo池不为空，新增该utxo的amount
+    if (this.utxos[utxo.pubKey] != null) {
+      this.utxos[utxo.pubKey] = {amount: this.utxos[utxo.pubKey].amount + utxo.amount}
+      // 若该utxo为utxo池第一个utxo，则令该utxo的amount等于交易池amount
+    } else {
+      this.utxos[utxo.pubKey] = {amount: utxo.amount}
+    }
+  }
 
   // 将当前 UXTO 的副本克隆
-  clone() {}
+  clone() {
+   return this.utxos
+  }
 }
 
 export default UTXOPool
