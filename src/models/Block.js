8c74590d96788a0a49b2e5c98a6b32f8ab118ff1
JR <1359684233@qq.com>
2023-06-21 18:50:31 +0800

    final

--- src/models/Block.js
+++ src/models/Block.js
@@ -19,6 +19,7 @@ class Block {
     this.utxoPool = new UTXOPool({});
     this.merkleTree = new MerkleTree([new Transaction(0, this.coinbaseBeneficiary, 12.5)]);
     this.merkleTreeRoot = this.merkleTree.getRoot();
+    this.signature = '';
   }
 
   getPreviousBlock() {
@@ -53,9 +54,54 @@ setHash() {
   /**
    * 需包含 UTXOPool 的更新与 hash 的更新
    */
-  addTransaction(transaction) {
+  addTransaction(transaction, privateKey) {
+    // 验证交易签名
+    if (!transaction.verifySignature()) {
+      throw new Error('Transaction signature is not valid')
+    }
+    // 将交易添加到默克尔树中
     this.merkleTree.addNode(transaction)
+    // 更新 UTXO 池
     this.utxoPool.handleTransaction(transaction)
+
+    // 如果有私钥，则为区块添加签名
+    if (privateKey) {
+      const key = ec.keyFromPrivate(privateKey)
+      const signature = key.sign(this.hash, 'base64')
+      this.signature = signature.toDER('hex')
+    }
+  }
+
+  isValidTransaction(transaction) {
+    // 验证交易签名
+    if (!transaction.verifySignature()) {
+      return false
+    }
+    // 验证交易的输入是否都在 UTXO 池中
+    const inputs = transaction.inputs
+    for (let i = 0; i < inputs.length; i++) {
+      const input = inputs[i]
+      const utxo = this.utxoPool.getUTput(new UTXO(input.prevTxHash, input.outputIndex))
+      if (!utxo || utxo.address !== input.address || utxo.amount < input.amount) {
+        return false
+      }
+    }
+    // 验证交易的输出是否合法
+    const outputs = transaction.outputs
+    let totalOutput = 0
+    for (let i = 0; i < outputs.length; i++) {
+      const output = outputs[i]
+      if (output.amount <= 0) {
+        return false
+      }
+      totalOutput += output.amount
+    }
+    // 验证交易的输入和输出是否平衡
+    const totalInput = transaction.inputs.reduce((total, input) => total + input.amount, 0)
+    if (totalInput < totalOutput) {
+      return false
+    }
+    return true
   }
 }
 
--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,22 +1,56 @@
 import sha256 from 'crypto-js/sha256.js'
+import EC from 'elliptic'
+
+const ec = new EC.ec('secp256k1')
 
 class Transaction {
   constructor(miner, receiverPubKey, num) {
     this.miner = miner
     this.receiverPubKey = receiverPubKey
     this.num = num
+    this.signature = ''
     this._setHash()
-}
-// 更新交易 hash
-_setHash() {
-this.hash = this._calculateHash();
-}
+  }
 
-// 计算交易 hash 的摘要函数
-_calculateHash() {
-return sha256(this.receiverPubKey + this.num).toString()
-}
+  // 更新交易 hash
+  _setHash() {
+    this.hash = this._calculateHash();
+  }
 
+  // 计算交易 hash 的摘要函数
+  _calculateHash() {
+    return sha256(this.receiverPubKey + this.num).toString()
+  }
+
+  // 签名函数
+  sign(privateKey) {
+    // 根据私钥生成公钥
+    const key = ec.keyFromPrivate(privateKey)
+    const publicKey = key.getPublic('hex')
+    // 确认签名者和交易发送者是否一致
+    if (publicKey !== this.miner) {
+      throw new Error('You cannot sign transactions for other miners')
+    }
+    // 计算交易 hash 的摘要
+    const transactionHash = this._calculateHash()
+    // 用私钥对摘要进行签名
+    const signature = key.sign(transactionHash, 'base64')
+    this.signature = signature.toDER('hex')
+  }
+
+  // 验签函数
+  hasValidSignature() {
+    // 如果没有签名，说明交易无效
+    if (!this.signature) {
+      return false
+    }
+    // 根据公钥生成一个验证器
+    const key = ec.keyFromPublic(this.miner, 'hex')
+    // 计算交易 hash 的摘要
+    const transactionHash = this._calculateHash()
+    // 验证签名是否正确
+    return key.verify(transactionHash, this.signature)
+  }
 }
 
 export default Transaction
